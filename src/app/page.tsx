fe7cf0e65bb77d99cd3cc2a9543591c81d366168
Nippun Tyagi <nippuntyagi@gmail.com>
2024-05-13 22:48:58 +0530

    weather api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client"
+
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import { WeatherData } from "@/interfaces/weather";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>('WeatherData', async () =>
+    {
+      const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`)
+      return data
+    }
+  )
+  if (isLoading) return (
+    <div className="flex item-center min-h-screen justify-center">
+      <p className="animate-bounce">
+        loading...
+      </p>
+    </div>
+  )
+
   return (
     <div className="flex flex-col gap-4 bg-grey-100 min-h-screen">
       <Navbar />
